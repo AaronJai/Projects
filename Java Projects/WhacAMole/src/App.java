ae998ae242526ef01aa19bd26ec295aaeeed514d
Aaron <aaron.tan1@gmail.com>
2024-02-10 20:44:52 +0800

    First session commit

--- Java Projects/WhacAMole/src/App.java
+++ Java Projects/WhacAMole/src/App.java
@@ -1,5 +1,9 @@
+import javax.swing.ImageIcon;
+
 public class App {
     public static void main(String[] args) throws Exception {
         WhacAMole WhacAMole = new WhacAMole();
+        
+
     }
 }
--- Java Projects/WhacAMole/src/WhacAMole.java
+++ Java Projects/WhacAMole/src/WhacAMole.java
@@ -6,12 +6,25 @@ import javax.swing.*;
 public class WhacAMole {
     int boardWidth = 600;
     int boardHeight = 650;
-    
+
+    ImageIcon moleIcon;
+    ImageIcon plantIcon;
     JFrame frame = new JFrame("WhacAMole");
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    JButton[] board = new JButton[9];
 
     WhacAMole() {
+
+        try {
+            // Use the native macOS look and feel
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
@@ -26,6 +39,30 @@ public class WhacAMole {
 
         textPanel.setLayout(new BorderLayout());
         textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(3, 3));
+        frame.add(boardPanel);
+        boardPanel.setBackground(Color.black);
+
+        // plantIcon = new ImageIcon(getClass().getResource("piranha.png"));
+        //  This approach of loading resources is recommended.
+        Image plantImg = new ImageIcon(getClass().getResource("piranha.png")).getImage();
+        plantIcon = new ImageIcon(plantImg.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH));
+        
+        for (int i = 0; i < 9; i++) {
+            JButton tile = new JButton();
+            board[i] = tile;
+            boardPanel.add(tile);
+            tile.setIcon(plantIcon);
+            
+
+            // tile.setBackground(Color.darkGray);
+            // tile.setForeground(Color.white);
+            tile.setFocusable(false);
+
+                
+        }
     }
 
 }
