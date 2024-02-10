8e2720a28f0c865d5ff1c909f892d4fa9f364a51
Aaron <aaron.tan1@gmail.com>
2024-02-10 19:08:18 +0800

    first initialisation

--- Java Projects/WhacAMole/src/WhacAMole.java
+++ Java Projects/WhacAMole/src/WhacAMole.java
@@ -1,3 +1,24 @@
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+
+
 public class WhacAMole {
+    int boardWidth = 600;
+    int boardHeight = 650;
     
+    JFrame frame = new JFrame("WhacAMole");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+
+    WhacAMole() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
+
 }
