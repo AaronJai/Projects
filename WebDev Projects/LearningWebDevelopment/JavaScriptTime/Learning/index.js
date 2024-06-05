86286846257895185367a87c9ba2dd9b939bfe3e
Aaron <aaron.tan1@gmail.com>
2024-06-05 14:04:14 +0800

    rest parameters

--- WebDev Projects/LearningWebDevelopment/JavaScriptTime/Learning/index.js
+++ WebDev Projects/LearningWebDevelopment/JavaScriptTime/Learning/index.js
@@ -311,3 +311,44 @@ switch (true) {
 // console.log(foods2);                        
 
 //--------------------------------------------------------------------------------------------
+    /* REST PARAMETERS  ---- opposite of spread
+
+    (...rest) allows a function to work with a variable number of arguments
+    by bundling them into an array.
+    
+    spread = expands an array into separate elements
+    rest = bundles separate elements into an array
+    */
+
+function openFridge(...foods) {
+    console.log(...foods);                         // pizza ChickenNCheese Sushi  -- resting then spreading
+    console.log(foods);                            // (3) ['pizza', 'ChickenNCheese', 'Sushi']
+}    
+
+function getFood(...foods) {
+    return foods;
+}
+
+const food1 = "pizza";
+const food2 = "ChickenNCheese";
+const food3 = "Sushi";
+openFridge(food1, food2, food3);                    
+
+const foods = getFood(food1, food2, food3);
+console.log(foods);                                // (3) ['pizza', 'ChickenNCheese', 'Sushi']
+
+function sum(...numbers) {                         // same concept for e.g., finding average
+    let result = 0;
+    for (let number of numbers) {
+        result += number;
+    }
+    return result;
+}
+
+const total = sum(1, 2, 3, 4, 5);
+
+function combineStrings(...strings) {
+    return strings.join(" ");
+}
+
+const fullName = combineStrings("Mr", "Spongebob");
